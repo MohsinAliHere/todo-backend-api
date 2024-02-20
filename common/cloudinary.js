560bfb32630b4ca589ae73361d4b8a1b4ebe8c9d
MohsinAliHere~ <mohsinalihere@gmail.com>
2024-02-20 17:58:18 +0500

    crud operation done with authentication v1

---
+++ common/cloudinary.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from "cloudinary";
+
+const cloudinary = cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { cloudinary, upload };
--- database/index.js
+++ database/index.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const ConnectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_CONNECT);
+    const conn = await mongoose.connect(process.env.MONGODB_CONNECT , 
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
     console.log("Connect to MongoDB ..." , conn.connection.host);
   } catch (error) {
     console.log("Failed to connect", error);

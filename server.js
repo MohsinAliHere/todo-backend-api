b58d875e850238205c17c0a0ef1e93515441f3c6
MohsinAliHere~ <mohsinalihere@gmail.com>
2024-02-20 17:42:22 +0500

    crud operation done with authentication v1

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const ConnectDb = require("./database");
 const jsonResponseMiddleware = require("./middleware/responseApi");
-const app = express();
+const AuthRoutes = require("./routes/auth");
+const TodoRoutes = require("./routes/todo");
+
 const PORT = process.env.PORT || 3000;
-const AuthRoutes = require("./routes/auth")
-const TodoRoutes = require("./routes/todo")
+const app = express();
+const cors = require("cors");
 
 // connect to database
-ConnectDb()
+ConnectDb();
 
 // middleware
+
+app.use(cors("*"));
 app.use(
   express.json({
     urlenscode: true,
@@ -19,9 +23,10 @@ app.use(
 );
 app.use(jsonResponseMiddleware);
 
-
 //login - register - check auth routes
-app.use(AuthRoutes)
-app.use(TodoRoutes)
+app.use("/api/v1/", AuthRoutes);
+
+//create - read - delete - edit - delete all  todo routes
+app.use("/api/v1/", TodoRoutes);
 
 app.listen(PORT, () => console.log("Port listening on " + PORT));
